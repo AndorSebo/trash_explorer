b285d11dfac0952188db3b376b4c9905dcdc90f6
AndorSebo <andorsebo@gmail.com>
2017-10-10 16:43:13 +0200

    [Android] Regisztrációs felület létrehozva, átvezető animációk hozzáadva.

--- Android/TrashExplorer/app/src/main/java/balint/andor/trashexplorer/MainActivity.java
+++ Android/TrashExplorer/app/src/main/java/balint/andor/trashexplorer/MainActivity.java
@@ -1,6 +1,6 @@
 package balint.andor.trashexplorer;
 
-import android.provider.Settings;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,21 +14,23 @@ import balint.andor.trashexplorer.Classes.Global;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        overridePendingTransition(R.animator.scale_from_corner,R.animator.scale_to_corner);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
         TextView signUp = (TextView) findViewById(R.id.signUp);
         ActionProcessButton signInButton = (ActionProcessButton) findViewById(R.id.signIn);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Sign Up text clicked", Toast.LENGTH_SHORT).show();
+
+                openRegistration();
             }
         });
         signInButton.setOnClickListener(new View.OnClickListener() {
@@ -41,4 +43,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void openRegistration(){
+        Intent registration = new Intent(MainActivity.this, RegActivity.class);
+        startActivity(registration);
+        finish();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.animator.scale_from_corner,R.animator.scale_to_corner);
+    }
+
+
 }
